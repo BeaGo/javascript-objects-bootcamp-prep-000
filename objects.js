5cba661e8b99dc52ef1c3bc635c73bc99efbd640
Bea Go <yobeago@gmail.com>
2018-05-01 22:46:27 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = { Britney Spears: 'Oops!...I Did It Again' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = 'songTitle';
+  return playlist;
+}

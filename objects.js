0c4112fbb8a9a28f56aba0d3b52bc874e51af5dc
Bea Go <yobeago@gmail.com>
2018-05-01 23:44:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(obj, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
- delete playlist.artistName
+ delete playlist[artistName]
  return playlist
 }
 
